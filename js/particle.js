8caef2025055b03b04620cbdabe632034769847c
Michael Pham <michael@goldefy.com>
2022-11-11 19:09:33 +0700

    hihi

--- js/particle.js
+++ js/particle.js
@@ -3,11 +3,11 @@
  */
 var settings = {
   particles: {
-    length: 600, // maximum amount of particles
-    duration: 20, // particle duration in sec
+    length: 500, // maximum amount of particles
+    duration: 2, // particle duration in sec
     velocity: 100, // particle velocity in pixels/sec
     effect: -0.75, // play with this for a nice effect
-    size: 50, // particle size in pixels
+    size: 30, // particle size in pixels
   },
 };
 
